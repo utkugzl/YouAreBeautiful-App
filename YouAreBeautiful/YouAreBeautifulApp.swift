a02924e1c5ea015c5700fb0b50673d7911a53191
Utku Guzel <utkugzl@icloud.com>
2023-06-29 19:40:49 +0300

    Initial Commit

---
+++ YouAreBeautiful/YouAreBeautifulApp.swift
@@ -0,0 +1,17 @@
+//
+//  YouAreBeautifulApp.swift
+//  YouAreBeautiful
+//
+//  Created by Utku Güzel on 29.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct YouAreBeautifulApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
